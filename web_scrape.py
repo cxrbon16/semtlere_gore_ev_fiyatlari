2bc2eb9e5376361defa13d2ff1b5758dd57f5092
Yavuz Selim Aygan <ayganyavuz@gmail.com>
2022-10-30 22:44:28 +0300

    Add files via upload

--- web_scrape.py
+++ web_scrape.py
@@ -2,6 +2,7 @@
 you receive the data from the web-site here
 '''
 import requests
+import csv
 from bs4 import BeautifulSoup
 
 headers = {'User-Agent': 'Mozilla/5.0 (X11; U; Linux i686; en-US) AppleWebKit/534.3 (KHTML, like Gecko) Chrome/6.0.472.63 Safari/534.3'}
@@ -9,12 +10,11 @@ main_url = 'https://www.emlakjet.com/satilik-konut/istanbul/'
 links = []
 last_index=0
 page = 0
-while(page < 10):
+while(page < 150):
     if page == 0:
         req = requests.get(main_url, headers=headers)
     else:
         req = requests.get(main_url + str(page), headers=headers)
-
     soup = BeautifulSoup(req.content, 'html.parser')
     things = soup.find_all('div', class_='_3qUI9q')
     for i in things:
@@ -33,20 +33,23 @@ for i in links:
     soup_det = BeautifulSoup(req_det.content, 'html.parser')
     loc = soup_det.find('div', class_='_3VQ1JB').find('p').text
     i.append(loc)
+    price = soup_det.find('div', class_='_2TxNQv').text
+    print(price)
+    i.append(price)
     features = soup_det.find_all('div', class_='_35T4WV')
     index1 = 0
     for j in features:
         index1 = index1 + 1
         detail = j.findAll('div', class_='_1bVOdb')
-        print(detail[1].text, index1)
-        if index1 == 5: i.append(detail[1].text)
-        if index1 == 9: i.append(detail[1].text)
-        if index1 == 20: i.append(detail[1].text)
-        if index1 == 22: i.append(detail[1].text)
+        if detail[0].text == "Binanın Yaşı": i.append(detail[1].text)
+        if detail[0].text == "Yapı Durumu": i.append(detail[1].text)
+        if detail[0].text == "Net Metrekare": i.append(detail[1].text)
 
 print(links)
 
+fields = ["index", "title", "link", "district", "age", "newness", "squaremeters"]
 
-'''
-[index, title ,semt ,m2 ,floor , oda, yaş]
-'''
+with open('GFG', 'w', encoding="utf-8") as f:
+    write = csv.writer(f)
+    write.writerow(fields)
+    write.writerows(links)
